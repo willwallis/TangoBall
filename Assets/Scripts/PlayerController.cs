3358bc5585e778d44f36c6f8c277dd4c598dc0f5
Will <will@knewto.com>
2015-07-29 17:47:03 -0700

    Added collision detection, scoring, and win message. Mac build in build folder.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public Text winText;
 	public float speed;
+	public Text countText;
+
 	private Rigidbody rb;
+	private int count;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
 	}
 
 	// Before performing phsyics calculations
@@ -21,6 +29,26 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce(movement * speed);
 
 	}
+
+	// Code to detect pick ups object collisions
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.gameObject.CompareTag("Pick Up"))
+		   {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+			if(count == 12)
+				{
+				winText.text = "You Win";
+				}
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Score: " + count.ToString ();
+	}
+
 	// Before rendering a frame
 	void Update ()
 	{
